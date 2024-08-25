3db46b2e0ac8928d88978c8654c41e22effb2042
Sukhdev Patil <sukhadevpatil@gmail.com>
2024-08-25 23:48:17 +0530

    first commit

--- src/contest/FinalArrayStateKMultiplications.java
+++ src/contest/FinalArrayStateKMultiplications.java
@@ -1,2 +1,35 @@
-package contest;public class FinalArrayStateKMultiplications {
+package contest;
+
+import java.util.Arrays;
+
+public class FinalArrayStateKMultiplications {
+
+    public static void main(String[] args) {
+        int[] nums = {2,1,3,5,6};
+        int k = 5;
+        int multiplier = 2;
+
+        int[] result = getFinalState(nums, k, multiplier);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] getFinalState(int[] nums, int k, int multiplier) {
+
+        for(int j = k; j > 0; j--) {
+            int minValIndex = 0;
+            int minVal = nums[0];
+            for(int i = 1; i < nums.length; i++) {
+                if (nums[i] < minVal) {
+                    minVal = nums[i];
+                    minValIndex = i;
+                }
+            }
+
+            System.out.println(Arrays.toString(nums));
+
+            nums[minValIndex] = nums[minValIndex] * multiplier;
+        }
+
+        return nums;
+    }
 }
